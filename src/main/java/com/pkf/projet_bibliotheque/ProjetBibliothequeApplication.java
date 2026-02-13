802c1d0f900016b07c9b7d28b993ff605e101f0b
teddy <teddykomta2001@gmail.com>
2026-02-13 14:51:19 +0100

    projet de gestion bibliothèque

---
+++ src/main/java/com/pkf/projet_bibliotheque/ProjetBibliothequeApplication.java
@@ -0,0 +1,13 @@
+package com.pkf.projet_bibliotheque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetBibliothequeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetBibliothequeApplication.class, args);
+	}
+
+}
