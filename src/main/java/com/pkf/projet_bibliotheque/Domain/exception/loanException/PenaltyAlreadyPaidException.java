7aae5d28baa56f52e1ec7ed298cbcc6ee27eaa3f
djemmo kounga anne <djemmokoungaanne@gmail.com>
2026-02-18 09:39:34 +0100

    feature: modification des exceptions

--- src/main/java/com/pkf/projet_bibliotheque/Domain/exception/loanException/PenaltyAlreadyPaidException.java
+++
@@ -1,7 +0,0 @@
-package com.pkf.projet_bibliotheque.Domain.exception.loanException;
-
-public class PenaltyAlreadyPaidException extends RuntimeException {
-    public PenaltyAlreadyPaidException(String message) {
-        super(message);
-    }
-}
